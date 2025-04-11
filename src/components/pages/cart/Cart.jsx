e5774a92da31d181a40fff61e950b8a34fd8c24e
Sky <akpakash490@gmail.com>
2025-04-11 20:57:47 +0530

    cart component

--- src/components/pages/cart/Cart.jsx
+++ src/components/pages/cart/Cart.jsx
@@ -6,20 +6,22 @@ const Cart = () => {
   const { cart, decrementItem, addToCart, total } = useProductDetail();
 
   return (
-    <div>
-      <h1>Your Cart</h1>
-      {Object.values(cart).map((item) => (
-        <div key={item?.product?.id}>
-            
-          <h3>{item?.product?.title}</h3>
-          <p>Price: ${item?.product?.price}</p>
-          <p>Quantity: {item.quantity}</p>
+    <div className="cart-container">
+    <h1>Your Cart</h1>
+    {Object.values(cart).map((item) => (
+      <div className="cart-item" key={item?.product?.id}>
+        <h3 className="item-title">{item?.product?.title}</h3>
+        <p className="item-price">Price: ${item?.product?.price}</p>
+        <p className="item-quantity">Quantity: {item.quantity}</p>
+        <div className="item-controls">
           <button onClick={() => decrementItem(item.product.id)}>-</button>
           <button onClick={() => addToCart(item.product.id)}>+</button>
         </div>
-      ))}
-      <h3>Total: ${total}</h3>
-    </div>
+      </div>
+    ))}
+    <h3 className="cart-total">Total: ${total}</h3>
+  </div>
+  
   );
 };
 
